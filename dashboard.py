645684fcbbd6867eaec34bf5b6b9c0e110cea35f
FilipMajchrzak <f.majc@outlook.com>
2026-09-09 17:19:54 +0100

    Add Streamlit skeleton app

---
+++ dashboard.py
@@ -0,0 +1,6 @@
+#Streamlit app (layout + calls the other files)
+import streamlit as st
+
+st.title("Stock Analysis Dashboard")
+st.write("If you are seeing this, the app is running successfully!")
+
